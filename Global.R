26d4eb0a1678c268d1b8d5e99c3835f4f1b173ac
SCAVINO Martial <mscavino@pc-milene.clb.loc>
2022-10-14 15:17:51 +0200

    Feature plot autocompletion, alpha sur scatter

--- Global.R
+++ Global.R
@@ -3,6 +3,8 @@ setwd("/Users/mscavino/PreprocessingComparison/")
 
 library(shiny)
 library(shinyWidgets)
+library(shinysky)
+
 library(Seurat)
 library(tidyverse)
 library(dplyr)
@@ -12,30 +14,68 @@ library(cowplot)
 library(viridis)
 library(DoubletFinder)
 
+
 seu <- Read10X("data/filtered_feature_bc_matrix/")
 
 
 data <- CreateSeuratObject(seu)
 
+# Mets les noms des gènes en majuscule (à cause du cell cycle)
+rownames(data@assays$RNA@counts) <- toupper(rownames(data@assays$RNA@counts))
+rownames(data@assays$RNA@data) <- toupper(rownames(data@assays$RNA@data))
+
 
 data <- NormalizeData(data,verbose = FALSE)
+
 
 # Find and scale variable genes
 data <- FindVariableFeatures(data, selection.method = "vst", nfeatures = 2000, verbose = FALSE)
 data <- ScaleData(data,features = rownames(data))
 
 
-data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^mt-")
+data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
 data[["Quality"]] <- "Good"
 
 
+# Cell cycle
+s.genes <- cc.genes$s.genes
+g2m.genes <- cc.genes$g2m.genes
 
+data <- CellCycleScoring(data, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+
+
+# Dimension reduction
 data <- RunPCA(data)
 data <- RunTSNE(data)
 data <- RunUMAP(data, dims = 1:50)
 
 
+# Stocke les coordonnées des cellules sur la UMAP pour faire des ggplot
+data@meta.data["UMAP_1"] <- data[["umap"]]@cell.embeddings[,1]
+data@meta.data["UMAP_2"] <- data[["umap"]]@cell.embeddings[,2]
 
+data@meta.data["TSNE_1"] <- data[["tsne"]]@cell.embeddings[,1]
+data@meta.data["TSNE_2"] <- data[["tsne"]]@cell.embeddings[,2]
+
+data@meta.data["PCA_1"] <- data[["pca"]]@cell.embeddings[,1]
+data@meta.data["PCA_2"] <- data[["pca"]]@cell.embeddings[,2]
+
+ggplot(data@meta.data, aes(TSNE_1, TSNE_2, color = nFeature_RNA)) +
+  geom_point() + scale_color_viridis_b()
+
+ggplot(data@meta.data, aes(TSNE_1, TSNE_2, color = percent.mt)) +
+  geom_point() + scale_color_viridis_c()
+
+# test <- data[["pca"]]@cell.embeddings
+# 
+# 
+# bizarre <- data@meta.data[names(test[which(test[,1] > 15 & test[,2] < 10), "PC_1"]),]
+# 
+# 
+# table(bizarre$Phase)
+# summary(bizarre$nFeature_RNA)
+# summary(bizarre$nCount_RNA)
+# summary(bizarre$percent.mt)
 
 
 ################## PLOTLY FUNCTION #################################
@@ -65,8 +105,7 @@ hline <- function(y = 0, color = "blue") {
 }
 
 
-
-
+genes <- data.frame("gene" = rownames(data))
 
 
 ################################## UI ##########################################
@@ -107,19 +146,28 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("Umap", plotOutput("umap")), 
         tabPanel("Tsne", plotOutput("tsne")),
-        tabPanel("Scatter", plotOutput("scatter_feature"), plotOutput("scatter_mt")),
+        tabPanel("Pca", plotOutput("pca")),
+        tabPanel("Scatter", plotOutput("scatter_mt"), plotOutput("scatter_feature")),
         tabPanel("Violin", plotOutput("violinplot")),
-        tabPanel("Histogramme", plotOutput("density")),
+        tabPanel("Histogramme", plotOutput("density_mt"), plotOutput("density_feature")),
         tabPanel("Plotly", plotlyOutput("test")),
-        tabPanel("FeaturePlot", textInput("gene", NULL,value = "", placeholder = "Veuillez rentrer un gène"),
+        
+        tabPanel("FeaturePlot",
+                 textInput.typeahead("gene", placeholder = "Veuillez rentrer un gène", 
+                                     local = genes, valueKey = "gene",
+                                     tokens =  c(1:length(genes$gene)),
+                 template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-gene'>{{gene}}</p>")),
                  plotOutput("featureplot"))
+        ,) #Fin tabsetpanel
         
        )
      )
-   )
 ), #Fin de la partie QC
 
   tabPanel("Normalisation",
+           
+           
+           # Un autre tab panel set avec d'autres entrée et d'autres plots en sortie
            
            
            
@@ -134,20 +182,16 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  
-  
+  ######################################## PARTIE QC
   
   # Violin plots
   output$violinplot <- renderPlot({
     
     a <- VlnPlot(data, "nCount_RNA") + NoLegend()
-    
     b <- VlnPlot(data, "nFeature_RNA") + 
       geom_hline(yintercept = input$features, col = "#832681") + NoLegend()
-    
     c <- VlnPlot(data, "percent.mt") + 
       geom_hline(yintercept = input$mt, col = '#FEB078') + NoLegend()
-    
     plot_grid(a, b, c, nrow = 1)
     
   })
@@ -157,9 +201,10 @@ server <- function(input, output, session) {
   output$scatter_mt <- renderPlot({
     
     ggplot(data@meta.data, aes(nCount_RNA, percent.mt, color = nFeature_RNA)) +
-      geom_point() + 
+      geom_point(data = data@meta.data[data$percent.mt > input$mt,], alpha = 0.2) + 
+      geom_point(data = data@meta.data[data$percent.mt <= input$mt,],) + 
       geom_hline(yintercept = input$mt, col = "#FEB078") + 
-      scale_color_viridis()
+      scale_color_viridis(option = "B") + theme_light()
     
   })
   
@@ -169,9 +214,11 @@ server <- function(input, output, session) {
   output$scatter_feature <- renderPlot({
     
     ggplot(data@meta.data, aes(nCount_RNA, nFeature_RNA, color = percent.mt)) +
-      geom_point() + 
+      geom_point(data = data@meta.data[data$nFeature_RNA < input$features[1],], alpha = 0.2) + 
+      geom_point(data = data@meta.data[data$nFeature_RNA > input$features[2],], alpha = 0.2) + 
+      geom_point(data = data@meta.data[data$nFeature_RNA <= input$features[2] & data$nFeature_RNA >= input$features[1],]) + 
       scale_color_viridis() +
-      geom_hline(yintercept = input$features, col = "#832681")
+      geom_hline(yintercept = input$features, col = "#832681") + theme_light()
       
     
   })
@@ -194,7 +241,7 @@ server <- function(input, output, session) {
     
     data$Quality <- "Good"
     data@meta.data[which(data$percent.mt> input$mt | data$nFeature_RNA < input$features[1] | data$nFeature_RNA > input$features[2]), "Quality"] <- "Bad"
-    DimPlot(data, reduction = "umap", group.by = "Quality", cols = c("#440154", "#21918c"))
+    DimPlot(data, reduction = "umap", group.by = "Quality", cols = c("#440154", "#87CEFA"))
     
   })
   
@@ -203,51 +250,70 @@ server <- function(input, output, session) {
     
     data$Quality <- "Good"
     data@meta.data[which(data$percent.mt> input$mt | data$nFeature_RNA < input$features[1] | data$nFeature_RNA > input$features[2]), "Quality"] <- "Bad"
-    DimPlot(data, reduction = "tsne", group.by = "Quality", cols = c("#440154", "#21918c"))
+    DimPlot(data, reduction = "tsne", group.by = "Quality", cols = c("#440154", "#87CEFA"))
     
   })
+  
+  # PCA du dataset groupée par celulles gardées ou non
+  
+  output$pca <- renderPlot({
+    
+    data$Quality <- "Good"
+    data@meta.data[which(data$percent.mt> input$mt | data$nFeature_RNA < input$features[1] | data$nFeature_RNA > input$features[2]), "Quality"] <- "Bad"
+    DimPlot(data, reduction = "pca", group.by = "Quality", cols = c("#440154", "#87CEFA"))
+    
+  })
+  
+  
   
   
   # Plot 3D qui permet d'afficher 4 variables d'un coup (nCount, nFeatures, %mt et si on garde la cellule ou pas)
   output$test <- renderPlotly({
     
-    
     data$Quality <- "Good"
     data@meta.data[which(data$percent.mt> input$mt | data$nFeature_RNA < input$features[1] | data$nFeature_RNA > input$features[2]), "Quality"] <- "Bad"
-    
     plot_ly(data@meta.data, x = ~nCount_RNA, y = ~nFeature_RNA, z = ~percent.mt, color = ~Quality) %>% 
       add_markers() %>% 
       layout(scene = list(xaxis = list(title = 'nCount'),
                           yaxis = list(title = 'nFeature'),
                           zaxis = list(title = 'percent.mt')))
-  })
+    
+    })
     
     
   # Histogramme et density plot des nFeatures
-  output$density <- renderPlot({
-    
+  output$density_feature <- renderPlot({
     
     ggplot(data@meta.data, aes(x = nFeature_RNA)) + 
-      geom_histogram(aes(y = ..density..), bins = 200, fill = "#FFDB6D", ) + 
-      geom_density(color = "#440154") + 
-      geom_vline(xintercept = input$features, col = "#832681")
-    
+      geom_histogram(aes(y = ..density..), bins = 200, fill = "#DDA0DD", ) + 
+      geom_density(color = "#8B0000") + 
+      geom_vline(xintercept = input$features, col = "#832681") + theme_light()
     
   })
+  
+  
+  # Histogramme et density plot des % mt
+  output$density_mt <- renderPlot({
+    
+    ggplot(data@meta.data, aes(x = percent.mt)) + 
+      geom_histogram(aes(y = ..density..), bins = 200, fill = "#FEB078", ) + 
+      geom_density(color = "#f8765c") + 
+      geom_vline(xintercept = input$mt, col = "#800000") + theme_light()
+    
+  })
+  
+  
   
   # Feature Plot à partir d'un gène donné
   
   output$featureplot <- renderPlot({
     
-    if (input$gene == "")
-    {
-      
-      
-    }
-    
+    if (input$gene == ""){}
     else {FeaturePlot(data, features = input$gene)}
     
     })
+  
+  ############################# FIN PARTIE QC 
   
   
 }
@@ -261,16 +327,16 @@ shinyApp(ui, server)
 
 
 
-########################## TEST ################################################
+ ########################## TEST ################################################
 ggplot(data@meta.data, aes(x = percent.mt)) + 
-  geom_histogram(aes(y = ..density..), bins = 200, fill = "#febb81", ) + 
+  geom_histogram(aes(y = ..density..), bins = 200, fill = "#FEB078", ) + 
   geom_density(color = "#f8765c") + 
-  geom_vline(xintercept = 50, col = "#d3436e")
+  geom_vline(xintercept = 50, col = "#EAB000")
 
 ggplot(data@meta.data, aes(x = nFeature_RNA)) + 
-  geom_histogram(aes(y = ..density..), bins = 200, fill = "#FFDB6D", ) + 
-  geom_density(color = "#440154") + 
-  geom_vline(xintercept = 50, col = "#832681")
+  geom_histogram(aes(y = ..density..), bins = 200, fill = "#DDA0DD", ) + 
+  geom_density(color = "#8B0000") + 
+  geom_vline(xintercept = 3000, col = "#832681") + theme_light()
 
 
 
